a747806017baf8b0ad0b29617cfba07ff22eb21d
xianxianwang <wangxianxian_sky@163.com>
2017-03-27 08:57:38 +0800

    Add files via upload
    work

---
+++ my.js
@@ -0,0 +1,9 @@
+ //my
+ function startMove(){
+	var x=document.getElementById("f1");
+	var y=document.getElementById("f2");
+	 x.style.animation="myFirst 4s";
+	 y.style.animation="mySecond 4s"
+ }
+
+
---
+++ mySecondHome.js
@@ -0,0 +1,33 @@
+function ImgBlock(){
+	document.getElementById("listImg").style.display="block";
+}
+function item31(){
+	document.getElementById("item31").style.display="block";
+	document.getElementById("item32").style.display="none";
+	document.getElementById("item33").style.display="none";
+}
+function item32(){
+	document.getElementById("item31").style.display="none";
+	document.getElementById("item32").style.display="block";
+	document.getElementById("item33").style.display="none";
+}
+function item33(){
+	document.getElementById("item31").style.display="none";
+	document.getElementById("item32").style.display="none";
+	document.getElementById("item33").style.display="block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
